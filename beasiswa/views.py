e979596629e012f5fe51de71aabf777673b3a970
rigifdaga <rfdauliaa@gmail.com>
2024-12-06 18:44:34 +0700

    add search views

--- beasiswa/views.py
+++ beasiswa/views.py
@@ -1,15 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from .models import Scholarship
 
-# GET semua data atau filter berdasarkan education_level
+# GET semua data atau filter berdasarkan education_level atau search
 def get_all_scholarships(request):
     education_level = request.GET.get('education_level', None)
+    query = request.GET.get('query', None)
+    
+    scholarships = Scholarship.objects.all()
+
     if education_level:
-        scholarships = Scholarship.objects.filter(education_level=education_level).values()
-    else:
-        scholarships = Scholarship.objects.all().values()
-    return JsonResponse(list(scholarships), safe=False)
+        scholarships = scholarships.filter(education_level=education_level)
+
+    if query:
+        scholarships = scholarships.filter(
+            Q(scholarship_name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(provider__icontains=query)
+        )
+
+    return JsonResponse(list(scholarships.values()), safe=False)
 
 # GET berdasarkan ID
 def get_scholarship_by_id(request, id):
