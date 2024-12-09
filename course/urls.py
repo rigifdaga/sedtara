8280402be158164ae75e5dd57131235d9cdaa2ed
Neo Koda <mneocicerok@gmail.com>
2024-12-10 02:47:20 +0700

    feat : module and material

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_all_courses
+from .views import get_all_courses, get_course_by_id, get_module_by_id
 
 urlpatterns = [
     path('course/', get_all_courses, name='get_all_courses'),
+    path('course/<int:course_id>/', get_course_by_id, name='get_course_by_id'),
+    path('course/<int:course_id>/<int:module_id>', get_module_by_id, name='get_module_by_id'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
-from .models import Course
+from .models import Course, Module
+from django.shortcuts import get_object_or_404
 
 def get_all_courses(request):
-    courses = Course.objects.select_related('teacher').all()
+    courses = Course.objects.select_related('teacher').order_by('-course_id')
 
     course_data = []
     for course in courses:
@@ -15,3 +16,42 @@ def get_all_courses(request):
         })
 
     return JsonResponse(course_data, safe=False)
+
+def get_course_by_id(request, course_id):
+    course = get_object_or_404(Course, course_id=course_id)
+
+    course_data = {
+        'course_id': course.course_id,
+        'course_name': course.course_name,
+        'teacher_full_name': course.teacher.full_name,
+        'course_description': course.course_description,
+        'modules': []
+    }
+
+    modules = course.module_set.all().order_by('module_id')
+    for module in modules:
+        course_data['modules'].append({
+            'module_id': module.module_id,
+            'module_name': module.module_name
+        })
+    
+    return JsonResponse(course_data)
+
+def get_module_by_id(request, course_id, module_id):
+    module = get_object_or_404(Module, module_id=module_id)
+
+    module_data = {
+        'module_id': module.module_id,
+        'module_name': module.module_name,
+        'materials': []
+    }
+
+    materials = module.materials_set.all().order_by('material_id')
+    for material in materials:
+        module_data['materials'].append({
+            'material_id': material.material_id,
+            'material_title': material.material_title,
+            'material_attachment': material.material_attachment
+        })
+    
+    return JsonResponse(module_data)
